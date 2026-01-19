74a3a5658a488fde6bb1c18e9c0dfe621f22d950
krishank1212 <105941613+krishank1212@users.noreply.github.com>
2026-01-19 23:00:05 +0000

    Add files via upload

---
+++ analytics.py
@@ -0,0 +1,28 @@
+import numpy as np
+import scipy.stats as stats
+def black_scholes(S0, K, r, sigma, T):
+    '''
+    Black-Scholes formula for European call option pricing.
+    
+    Parameters
+    ----------
+    S0 : float
+        Initial stock price
+    K : float
+        Strike price
+    r : float
+        Risk-free interest rate
+    sigma : float
+        Volatility
+    T : float
+        Time to maturity (years)
+        
+    Returns
+    -------
+    option_price : float
+        Theoretical option price
+    '''
+    d1 = (np.log(S0 /K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    call_price = S0 * stats.norm.cdf(d1) - K * np.exp(-r * T) * stats.norm.cdf(d2)
+    return call_price
